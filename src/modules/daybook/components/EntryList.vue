036240151e0c504f00ced325f50fea23d4bda2a8
BranBautista <bbrandon.abo@gmail.com>
2023-06-04 23:29:55 -0600

    entry view

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,9 +1,40 @@
 <template>
-  <h1>Entrylist</h1>
+    <div class="entry-list-container">
+        <div class="px-2 pt-2">
+            <input type="text"
+                class="form-control"
+                placeholder="Search"
+            >
+        </div>
+        <div class="entry-scrollarea">
+            <EntryElement
+                v-for="item in 100"
+                :key="item"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
+    import { defineAsyncComponent } from 'vue';
 
+    export default {
+        components: {
+            EntryElement: defineAsyncComponent(() => import('./EntryElement.vue'))
+        }
     }
 </script>
+
+<style lang="scss" scoped>
+    input{
+        height: 25px;
+    }
+    .entry-list-container{
+        border-right: 1px solid #2c3e50;
+        height: calc(100vh - 56px);
+    }
+    .entry-scrollarea{ 
+        height: calc(100vh - 110px);
+        overflow: scroll;
+    }
+</style>
